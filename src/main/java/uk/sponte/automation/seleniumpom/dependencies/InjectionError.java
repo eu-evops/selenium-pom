b16a8e98a975c12008e11e48d62e528da63301dd
Stanislaw Wozniak <swozniak@sponte.uk>
2015-04-17 10:54:52 +0100

    Injection error: new constructor accepting String

--- src/main/java/uk/sponte/automation/seleniumpom/dependencies/InjectionError.java
+++ src/main/java/uk/sponte/automation/seleniumpom/dependencies/InjectionError.java
@@ -1,10 +1,15 @@
 package uk.sponte.automation.seleniumpom.dependencies;
 
 /**
+ * Used to throw errors during dependency injection process.
  * Created by n450777 on 07/04/15.
  */
 public class InjectionError extends Error {
     public InjectionError(Throwable cause) {
         super(cause);
+    }
+
+    public InjectionError(String message) {
+        super(message);
     }
 }
